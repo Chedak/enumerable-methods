aeaf28f6c35cbfcbee8584dd737c50cd6f9a45f6
Alex <nikitin2009@gmail.com>
2019-02-11 17:50:57 +0200

    Add my_each test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,10 +1,16 @@
 require './methods'
 
 describe Enumerable do
-  context "#my_inject" do
+  describe "#my_inject" do
     it "multiplies the array elements" do
 
-      expect [1,2,3].my_inject{ |memo, item| memo * item }.to eql(6)
+      expect([1,2,3].my_inject{ |memo, item| memo * item }).to eql(6)
+
+    end
+  end
+
+  describe "#my_each" do
+    it "iterates through an array" do
 
     end
   end
