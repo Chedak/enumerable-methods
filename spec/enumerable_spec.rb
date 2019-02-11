55ed1952c1469408b96f679b5e426b373bf826b7
Chedak <cedomir.miskov@gmail.com>
2019-02-11 16:33:56 +0100

    add 1 test case to my_inject

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,1 +1,11 @@
 require './methods'
+
+describe Enumerable do
+  context "#my_inject" do
+    it "multiplies the array elements" do
+
+      expect [1,2,3].my_inject{ |memo, item| memo * item }.to eql(6)
+
+    end
+  end
+end
